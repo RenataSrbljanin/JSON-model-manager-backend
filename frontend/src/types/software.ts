83ef68dde4d8f48336b11944182141f3ed54a13b
RenataSrbljanin <renatasrbljanin@gmail.com>
2025-08-22 13:16:58 +0200

    pre velikog preokreta i FK sa installed_software

--- frontend/src/types/software.ts
+++ frontend/src/types/software.ts
@@ -2,8 +2,8 @@
 export interface Software {
   accepts_credentials: string[]; // 1 // Relationship
   compatible_data_types: string[]; //2
-  computer_idn: string; // 3
-  cpe_idn: string; // 4
+  computer_idn: string; // 3 = 1.1 + 1.2 + 1.3 => person_group_id:person_index:network_idn
+  cpe_idn: string; // 4         2
   hardware_ids: string[]; // 5
 
   idn: string; // 6
@@ -15,10 +15,10 @@ export interface Software {
   max_client_count: number; // 11
   network_clients: string[]; // 12
   network_dependencies: string[]; // 13
-  network_idn: number[]; // 14
+  network_idn: number[]; // 14  1.3
   network_servers: string[]; // 15
-  person_group_id: string | null; // 16
-  person_index: number; // 17
+  person_group_id: string | null; // 16  1.1
+  person_index: number; // 17  1.2
   provides_network_services: string[]; // 18
   provides_services: string[]; // 19
   provides_user_services: string[]; // 20
